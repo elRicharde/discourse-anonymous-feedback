216c6990bdcdc782f0362e5489be2b1eacc5cfa7
elRicharde <52285099+elRicharde@users.noreply.github.com>
2026-02-03 18:01:17 +0100

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,10 @@
+# name: discourse-anonymous-feedback
+# about: Anonymous feedback form (door code) that sends PM to a group
+# version: 0.1
+# authors: Richard
+
+enabled_site_setting :anonymous_feedback_enabled
+
+after_initialize do
+  # Chapter 2: routes + controller
+end
