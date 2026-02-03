4afd1ca0fbe6a4c60349d4e00ca5dccf46890848
elRicharde <52285099+elRicharde@users.noreply.github.com>
2026-02-03 21:15:54 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-anonymous-feedback
 # about: Anonymous feedback form (door code) that sends PM to a group
-# version: 0.2
+# version: 0.3
 # authors: Richard
 
 enabled_site_setting :anonymous_feedback_enabled
@@ -10,11 +10,21 @@ after_initialize do
     PLUGIN_NAME = "discourse-anonymous-feedback"
   end
 
-  require_relative "app/controllers/anonymous_feedback_controller"
+  # Load controller
+  require_dependency File.expand_path("../app/controllers/anonymous_feedback_controller.rb", __FILE__)
 
+  # Routes
   Discourse::Application.routes.append do
-    get "/anonymous-feedback" => "anonymous_feedback#index"
+    get  "/anonymous-feedback" => "anonymous_feedback#index"
     post "/anonymous-feedback" => "anonymous_feedback#create"
   end
-end
 
+  # Ensure Rails can find plugin views for this controller
+  # (needed in some Discourse/Rails setups when using custom controllers)
+  view_path = File.expand_path("../app/views", __FILE__)
+  ActiveSupport.on_load(:action_controller) do
+    if defined?(::AnonymousFeedbackController)
+      ::AnonymousFeedbackController.prepend_view_path(view_path)
+    end
+  end
+end
