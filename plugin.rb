02107dd650b4dbe665b0ca0b742d496139d58257
elRicharde <52285099+elRicharde@users.noreply.github.com>
2026-02-03 19:56:25 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,19 +1,3 @@
-class AnonymousFeedbackController < ApplicationController
-  skip_before_action :check_xhr, only: [:index]
-  skip_before_action :preload_json, only: [:index]
-  skip_before_action :redirect_to_login_if_required, only: [:index]
-  skip_before_action :verify_authenticity_token, only: [:create]
-  
-  def index
-    # Dein Code hier
-  end
-  
-  def create
-    # Dein Code zum Senden der Nachricht
-  end
-end
-
-
 # name: discourse-anonymous-feedback
 # about: Anonymous feedback form (door code) that sends PM to a group
 # version: 0.2
@@ -22,8 +6,31 @@ end
 enabled_site_setting :anonymous_feedback_enabled
 
 after_initialize do
-  require_dependency File.expand_path("../app/controllers/anonymous_feedback_controller.rb", __FILE__)
+  # Controller-Definition innerhalb des after_initialize Blocks
+  class ::AnonymousFeedbackController < ::ApplicationController
+    skip_before_action :check_xhr, only: [:index, :create]
+    skip_before_action :preload_json, only: [:index, :create]
+    skip_before_action :redirect_to_login_if_required, only: [:index, :create]
+    skip_before_action :verify_authenticity_token, only: [:create]
+    
+    def index
+      # Zeige das Formular
+      render json: { success: true }
+    end
+    
+    def create
+      # Verarbeite das Feedback
+      door_code = params[:door_code]
+      message = params[:message]
+      
+      # Hier kannst du den Code zur Verarbeitung einfügen
+      # z.B. PM an eine Gruppe senden
+      
+      render json: { success: true }
+    end
+  end
   
+  # Routes registrieren
   Discourse::Application.routes.append do
     get "/anonymous-feedback" => "anonymous_feedback#index"
     post "/anonymous-feedback" => "anonymous_feedback#create"
