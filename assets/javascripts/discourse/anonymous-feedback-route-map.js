cb213300cfe5343cda8cdc1f44199b6dde0452e4
Richard <richardgruhle@gmail.com>
2026-02-04 20:14:15 +0100

    bugixing

--- assets/javascripts/discourse/anonymous-feedback-route-map.js
+++ assets/javascripts/discourse/anonymous-feedback-route-map.js
@@ -1,7 +1,4 @@
-import { apiInitializer } from "discourse/lib/api";
-
-export default apiInitializer("1.8.0", (api) => {
-  // Public pages
-  api.addRoute("anonymous-feedback", { path: "/anonymous-feedback" });
-  api.addRoute("white-board", { path: "/white-board" });
-});
+export default {
+  resource: "anonymous-feedback",
+  path: "/anonymous-feedback",
+};
---
+++ assets/javascripts/discourse/white-board-route-map.js
@@ -0,0 +1,4 @@
+export default {
+  resource: "white-board",
+  path: "/white-board",
+};
